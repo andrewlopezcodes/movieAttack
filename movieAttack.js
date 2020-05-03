83910d63220c0883a1ac70dcdc8c82e74270080d
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-05-03 18:56:44 -0400

    movieAttack.js - created a debouncing event function for the input value received from the input tag.

--- movieAttack.js
+++ movieAttack.js
@@ -1,3 +1,5 @@
+/* This is the search widget */
+
 const fetchData = async (searchTerm) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
@@ -8,19 +10,31 @@ const fetchData = async (searchTerm) => {
   console.log(response.data);
 }
 
-
+/* once the user types into the input field: 
+    1) the event listener is triggered and calls debounce with arguments of the variable finishTypingSearchTerm and a time in miliseconds of how long to wait to send data to API.
+    2) finishTypingSearchTerm variable is set to a function that takes in an arguement of event ( the change in the input being listened for by the eventlistener 'input') and stores the value of the input tag in the html.
+    3) debounce function declares a local variable called setTimeoutId and returns a function that takes in all the arguments from func (the function passed in from finishTypingSearchTerm).
+    4) With each key press inside the input, the if statement is evaluated. On the first keystroke the if statement will evaluate to be false setting the variable setTimeoutId to the setTimeout id. On the next key stroke if there is a setTimeoutId, the if statement will evaluate true and clear the setTimeoutId.
+    5) on the last keystroke in the input tag, there will be an uncleared setTimeout Id and a delay timer set to the delay argument passed in by the argments gotten from event listener. Currently 1000 miliseconds will pass into the debounce function, which will be set in the settimeout second argument of the false response of the if statement. Which means there will be a 1 second delay before the fetchData(event.target.value) will be sent to the API using the fetchData function above.
+     */
 const input = document.querySelector('input');
+const debounce = (func, delay = 1000) => {
+  let setTimeoutId;
+  return (...args) => {
+    if (setTimeoutId) {
+      clearTimeout(setTimeoutId);
+    }
+    setTimeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay)
+  };
+};
 
-let setTimeoutId;
+
 
 const finishTypingSearchTerm = event => {
-  if (setTimeoutId) {
-    clearTimeout(setTimeoutId);
-  }
-  setTimeoutId = setTimeout(() => {
-    fetchData(event.target.value); //this will become the searchTearm in the fetchData async callback function
-  }, 500)
-}
+  fetchData(event.target.value); //this will become the searchTearm in the fetchData async callback function
+};
 
 
-input.addEventListener('input', finishTypingSearchTerm);
+input.addEventListener('input', debounce(finishTypingSearchTerm, 850));
