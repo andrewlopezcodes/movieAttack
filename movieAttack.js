ad963785bad1b801daa97e1a5d8429e370fbed7d
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-05-05 09:49:04 -0400

    index.html - added autocomplete script tag - movieAttack.js - removed code that dealt with the functionality of the dropdown - autocomplete.js - added all code removed from movieAttack.js to the function createAutoComplete.

--- movieAttack.js
+++ movieAttack.js
@@ -14,61 +14,6 @@ const fetchData = async (searchTerm) => {
   return response.data.Search;
 }
 
-const temporarySearchDisplay = document.querySelector('.temporarysearchresultsdisplay');
-temporarySearchDisplay.innerHTML = `
-  <label><b>Search For A Movie</b></label>
-  <input type = "input" />
-  <div class = "dropdown" >
-    <div class = "dropdown-menu">
-      <div class = "dropdown-content results" ></div>
-      </div>
-    </div>
-`;
-
-const input = document.querySelector('input');
-const searchResultsDropdown = document.querySelector('.dropdown');
-const showUserTemporaryResults = document.querySelector('.results');
-
-
-const finishTypingSearchTerm = async event => {
-  const movies = await fetchData(event.target.value); //this will become the searchTearm in the fetchData async callback function
-
-  if (!movies.length) {
-    searchResultsDropdown.classList.remove('is-active');
-    return;
-  }
-
-  showUserTemporaryResults.innerHTML = '';
-
-  searchResultsDropdown.classList.add('is-active');
-  for (let fetchedMovies of movies) {
-    const searchResultsDropdownOption = document.createElement('a');
-    const posterImageDecision = fetchedMovies.Poster === 'N/A' ? '' : fetchedMovies.Poster;
-
-    searchResultsDropdownOption.classList.add('dropdown-item');
-    searchResultsDropdownOption.innerHTML =
-      `
-      <img src= "${posterImageDecision} " />
-      <h7> ${fetchedMovies.Title}</h7>
-    `;
-
-    searchResultsDropdownOption.addEventListener('click', () => {
-      searchResultsDropdown.classList.remove('is-active');
-      input.value = fetchedMovies.Title;
-      clickedResultInDropdown(fetchedMovies);
-    });
-    showUserTemporaryResults.appendChild(searchResultsDropdownOption);
-  }
-};
-
-
-input.addEventListener('input', debounce(finishTypingSearchTerm, 850));
-
-document.addEventListener('click', event => {
-  if (!temporarySearchDisplay.contains(event.target)) {
-    searchResultsDropdown.classList.remove('is-active');
-  }
-});
 
 const clickedResultInDropdown = async moviefromdropdownlist => {
   const response = await axios.get('http://www.omdbapi.com/', {
